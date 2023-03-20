1ed1e43e650d042b6321dd0d562497e5b1df027c
Kyle Phillips <kphillips375@yahoo.com>
2023-03-19 21:22:41 -0400

    Moved Featered Products to a component. Will need SSR in the future

---
+++ src/components/FeaturedProducts.tsx
@@ -0,0 +1,27 @@
+// TODO: Setup ssr for lazy loading
+
+const featureProducts = [
+    {
+      id: 1,
+      name: 'Product 1',
+      image: 'https://dummyimage.com/200x200/000/fff',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      image: 'https://dummyimage.com/200x200/000/fff',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      image: 'https://dummyimage.com/200x200/000/fff',
+      price: 300,
+    },
+  ];
+  
+  export default function FeatureProducts() {
+    return featureProducts;
+  }
+  
